07c8ed2838f5502607e88959351f349d0153d3fa
sulyvan <sulyvan.weppe@gmail.com>
2022-01-11 23:00:22 +0100

    manage to update the type of the card, but remains to pass the data to this latest

---
+++ src/Components/MyFamily/ChildCard.js
@@ -0,0 +1,60 @@
+import { Button, Card, CardActionArea, CardContent, CardMedia, Avatar, Divider, Typography, Grid } from '@mui/material';
+import React from 'react'
+import { Edit } from '@mui/icons-material'
+import ChildInfoEdit from './ChildInfoEdit';
+import AddFamilyMemberDialog from './AddFamilyMemberDialog';
+
+
+class ChildCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            editChildDialogOpen: false
+        };
+    }
+
+    handleClick = () => {this.setState({editChildDialogOpen:true});}
+
+    handleDialogClose = () => {this.setState({editChildDialogOpen:false});}
+
+
+    render () {
+        return (
+            <div>
+                <Card sx={{marginTop:'2%'}}>
+                    <CardActionArea>
+                        <CardMedia >
+                            <Grid container>
+                                <Grid item xs={0} sx={{marginTop:'0.5%', marginBottom:'0.5%', marginLeft:'0.5%'}}>
+                                    <Avatar sx={{ backgroundColor:this.props.color}}>C</Avatar>
+                                </Grid>
+                                <Grid item xs={4} sx={{textAlign:'left', marginLeft:'1%', marginTop:'0.5%', marginBottom:'0.5%'}}>
+                                    <Typography variant="h6" sx={{textAlign:'left'}}>Camille Weppe-Ansart</Typography>
+                                </Grid>
+                                <Grid item xs={7} sx={{marginTop:'0.5%', marginBottom:'0.5%', textAlign:'right'}}>
+                                    <Button variant="contained" color="secondary" onClick={this.handleClick}>
+                                        Edit
+                                        <Edit sx={{marginLeft:'10%'}}/>
+                                    </Button>
+                                    <AddFamilyMemberDialog
+                                        isOpen={this.state.editChildDialogOpen}
+                                        onClose={this.handleDialogClose}
+                                        dialogColor={this.props.color}
+                                        memberType="child"
+                                        isTypeFrozen={true}/>
+                                </Grid>
+                            </Grid>
+                        </CardMedia>
+                        <Divider/>
+                        <CardContent>
+                            <ChildInfoEdit editEnabled={false}/>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            </div>
+        );
+    }
+}
+
+export default ChildCard;
